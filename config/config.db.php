eb2f757c72ef1329e177aa89761e64474f57e501
MoonFlock <new.grishin@gmail.com>
2021-03-05 15:16:10 +0300

    stressweb

---
+++ config/config.db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * STRESS WEB
+ * @author S.T.R.E.S.S.
+ * @copyright 2008 - 2012 STRESS WEB
+ * @version 13
+ * @web http://stressweb.ru  
+ */
+if (!defined('STRESSWEB')) die ('Access denied...');
+define("DBHOST", "localhost");
+define("DBUSER", "u1284666_default");
+define("DBPASS", "s6o_zCRl");
+define("DBNAME", "u1284666_default");
+define("COLLATE", "utf8");
+?>
